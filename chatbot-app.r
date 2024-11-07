dfba824e1a1abc7bb098542e6f66742911a27ba0
Bryan Milleanno <bryanmilleanno@gmail.com>
2024-11-07 20:02:01 +0100

    add: working simple chatbot input and response with ollama

--- chatbot-app.r
+++ chatbot-app.r
@@ -1,31 +1,53 @@
 # Load R packages
-library(pacman) 
-pacman::p_load(pacman, shiny, shinythemes) 
+renv::load()
+
+library(shiny)
+library(shinythemes)
+library(ollamar)
+
 
 # Define UI
-ui <- fluidPage(theme = shinytheme("cerulean"),
-                navbarPage(
-                  theme = "cerulean", 
-                  "My first webapp",
-                  tabPanel("Navbar 1",
-                           sidebarPanel(
-                             tags$h3("Input:"),
-                             textInput("txt1", "Given Name:", ""),
-                             textInput("txt2", "Surname:", ""),
-                           ),
-                           mainPanel(h1("Header 1"),
-                                     h4("Output 1"),
-                                     verbatimTextOutput("txtout"),
-                           )
-                  ),
-                  tabPanel("Navbar 2", "This bar is blank"),
-                )
+ui <- fluidPage(
+  theme = shinytheme("cerulean"),
+  titlePanel("Chatbot Example"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("user_input", "Your Message:", ""),
+      actionButton("send", "Send"),
+      br(), 
+      h5("Conversation History"), 
+      uiOutput("chat_history")
+    ),
+    mainPanel(
+      h4("Chatbot Model:"),
+      verbatimTextOutput("response", placeholder = TRUE),
+      
+      h4("Combined Input:"), 
+      verbatimTextOutput("txtout", placeholder = TRUE)
+    )
+  )
 )
 
 
 # Define server function  
 server <- function(input, output) {
+  test_connection()
+  chat_model <- "llama3.1"
   
+  models <- list_models()
+  model_name <- c(models[1][1,], models[1][2,])
+  output$response <- renderText({ model_name })
+  
+  
+  observeEvent(input$send, {
+    user_message <- input$user_input
+    print(paste("User message:", user_message))
+    
+    bot_response <- generate(chat_model, user_message, output="text", stream=TRUE)
+    print(paste("Bot response:", bot_response))
+    
+    output$txtout <- renderText({ bot_response })
+  })
   output$txtout <- renderText({
     paste( input$txt1, input$txt2, sep = " " )
   })
--- llm-test.r
+++ llm-test.r
@@ -9,6 +9,14 @@ renv::install("pacman")
 devtools::install_github("hauselin/ollamar")
 library(ollamar)
 test_connection()
+
 list_models()
 
+list_models <- list_models()
+print(list_models[1][1,])
+print(list_models[1][2,])
+print(c(list_models[1][1,], list_models[1][2,]))
+
 generate("llama3.1", "How are you today?", output="text", stream=TRUE) 
+
+cat("\014")
