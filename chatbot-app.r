61b4d99a690bc7e89507d6b700f5e6f33d80ce4b
Bryan Milleanno <bryanmilleanno@gmail.com>
2024-11-25 14:33:40 +0100

    fix: better flow for text input-output

--- chatbot-app.r
+++ chatbot-app.r
@@ -5,6 +5,9 @@ library(shiny)
 library(shinythemes)
 library(ollamar)
 
+# Initialization
+test_connection()
+chat_model <- "llama3.1"
 
 # Define UI
 ui <- fluidPage(
@@ -13,16 +16,10 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput("user_input", "Your Message:", ""),
-      actionButton("send", "Send"),
-      br(), 
-      h5("Conversation History"), 
-      uiOutput("chat_history")
+      actionButton("send", "Send")
     ),
     mainPanel(
-      h4("Chatbot Model:"),
-      verbatimTextOutput("response", placeholder = TRUE),
-      
-      h4("Combined Input:"), 
+      h4("Chatbox:"), 
       verbatimTextOutput("txtout", placeholder = TRUE)
     )
   )
@@ -30,26 +27,32 @@ ui <- fluidPage(
 
 
 # Define server function  
-server <- function(input, output) {
-  test_connection()
-  chat_model <- "llama3.1"
-  
+server <- function(input, output, session) {
   models <- list_models()
   model_name <- c(models[1][1,], models[1][2,])
-  output$response <- renderText({ model_name })
   
+  chat <- reactiveVal("")
+  bot_response <- reactiveVal("")
   
   observeEvent(input$send, {
-    user_message <- input$user_input
-    print(paste("User message:", user_message))
+    input_msg <- input$user_input
+    user_message <- paste0("User: ", input$user_input)
+    updated_chat <- paste(chat(), user_message, sep = "\n")
+    chat(updated_chat)
+    # clear input field
+    updateTextInput(session, "user_input", value = "")
+    print(paste("User message:", input_msg))
     
-    bot_response <- generate(chat_model, user_message, output="text", stream=TRUE)
-    print(paste("Bot response:", bot_response))
-    
-    output$txtout <- renderText({ bot_response })
+    # Generate bot response
+    bot_reply <- generate(chat_model, input_msg, output = "text")
+    bot_message <- paste0("Bot: ", bot_reply)
+    updated_chat <- paste(chat(), bot_message, sep = "\n")
+    chat(updated_chat)
+    print(paste("Bot response:", bot_reply))
   })
+  
   output$txtout <- renderText({
-    paste( input$txt1, input$txt2, sep = " " )
+    chat()
   })
 }
 
